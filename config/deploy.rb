11a6309ca8322a449f027040ebf8c6ab4a7e7246
max <jmao@brightcove.com>
2011-06-28 11:39:54 -0400

    Remove a d from hostname.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ role :db,  "your slave db-server here"
 namespace :deploy do
   task :start do 
     begin
-      run 'ssh aaa@ddd'
+      run 'ssh aaa@dd'
     rescue Exception => e
       logger.debug('failed')
     end
