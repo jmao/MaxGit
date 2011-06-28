ce53dd29df23662a02ccb8ec95cb294ef24bc270
max <jmao@brightcove.com>
2011-06-28 11:24:27 -0400

    Change aaa to ccc as ssh user name.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ role :db,  "your slave db-server here"
 namespace :deploy do
   task :start do 
     begin
-      run 'ssh aaa@bbb'
+      run 'ssh ccc@bbb'
     rescue Exception => e
       logger.debug('failed')
     end
